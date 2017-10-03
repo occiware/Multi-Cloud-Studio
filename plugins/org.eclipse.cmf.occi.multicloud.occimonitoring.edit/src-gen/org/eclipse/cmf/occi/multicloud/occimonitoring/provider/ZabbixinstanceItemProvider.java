344f0bc4122577b8f2b405ff27c87303e86bb0cd
fzalila <faiez.zalila@gmail.com>
2017-10-03 12:38:37 +0200

    add the org.eclipse.cmf.occi.multicloud.occimonitoring.edit.

---
+++ plugins/org.eclipse.cmf.occi.multicloud.occimonitoring.edit/src-gen/org/eclipse/cmf/occi/multicloud/occimonitoring/provider/ZabbixinstanceItemProvider.java
@@ -0,0 +1,223 @@
+/**
+ * Copyright (c) 2015-2017 Obeo, Inria
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 	
+ * Contributors:
+ * - William Piers <william.piers@obeo.fr>
+ * - Philippe Merle <philippe.merle@inria.fr>
+ * - Faiez Zalila <faiez.zalila@inria.fr>
+ */
+package org.eclipse.cmf.occi.multicloud.occimonitoring.provider;
+
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.cmf.occi.core.OCCIPackage;
+
+import org.eclipse.cmf.occi.core.provider.ResourceItemProvider;
+
+import org.eclipse.cmf.occi.multicloud.occimonitoring.OccimonitoringFactory;
+import org.eclipse.cmf.occi.multicloud.occimonitoring.OccimonitoringPackage;
+import org.eclipse.cmf.occi.multicloud.occimonitoring.Zabbixinstance;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+/**
+ * This is the item provider adapter for a {@link org.eclipse.cmf.occi.multicloud.occimonitoring.Zabbixinstance} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ZabbixinstanceItemProvider extends ResourceItemProvider {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ZabbixinstanceItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addZabbixinstancePortPropertyDescriptor(object);
+			addZabbixinstanceIdentifierPropertyDescriptor(object);
+			addZabbixinstanceIpPropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Zabbixinstance Port feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addZabbixinstancePortPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_Zabbixinstance_zabbixinstancePort_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_Zabbixinstance_zabbixinstancePort_feature", "_UI_Zabbixinstance_type"),
+				 OccimonitoringPackage.Literals.ZABBIXINSTANCE__ZABBIXINSTANCE_PORT,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Zabbixinstance Identifier feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addZabbixinstanceIdentifierPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_Zabbixinstance_zabbixinstanceIdentifier_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_Zabbixinstance_zabbixinstanceIdentifier_feature", "_UI_Zabbixinstance_type"),
+				 OccimonitoringPackage.Literals.ZABBIXINSTANCE__ZABBIXINSTANCE_IDENTIFIER,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Zabbixinstance Ip feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addZabbixinstanceIpPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_Zabbixinstance_zabbixinstanceIp_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_Zabbixinstance_zabbixinstanceIp_feature", "_UI_Zabbixinstance_type"),
+				 OccimonitoringPackage.Literals.ZABBIXINSTANCE__ZABBIXINSTANCE_IP,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This returns Zabbixinstance.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/Zabbixinstance"));
+	}
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String getText(Object object) {
+		String label = ((Zabbixinstance)object).getId();
+		return label == null || label.length() == 0 ?
+			getString("_UI_Zabbixinstance_type") :
+			getString("_UI_Zabbixinstance_type") + " " + label;
+	}
+	
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(Zabbixinstance.class)) {
+			case OccimonitoringPackage.ZABBIXINSTANCE__ZABBIXINSTANCE_PORT:
+			case OccimonitoringPackage.ZABBIXINSTANCE__ZABBIXINSTANCE_IDENTIFIER:
+			case OccimonitoringPackage.ZABBIXINSTANCE__ZABBIXINSTANCE_IP:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+	 * that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add
+			(createChildParameter
+				(OCCIPackage.Literals.ENTITY__PARTS,
+				 OccimonitoringFactory.eINSTANCE.createMetrics()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(OCCIPackage.Literals.ENTITY__PARTS,
+				 OccimonitoringFactory.eINSTANCE.createCpuusage()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(OCCIPackage.Literals.ENTITY__PARTS,
+				 OccimonitoringFactory.eINSTANCE.createMemoryusage()));
+	}
+
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getResourceLocator() {
+		return OccimonitoringEditPlugin.INSTANCE;
+	}
+
+}
---
+++ plugins/org.eclipse.cmf.occi.multicloud.occimonitoring.edit/src-gen/org/eclipse/cmf/occi/multicloud/occimonitoring/provider/ZabbixtemplateItemProvider.java
@@ -0,0 +1,223 @@
+/**
+ * Copyright (c) 2015-2017 Obeo, Inria
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 	
+ * Contributors:
+ * - William Piers <william.piers@obeo.fr>
+ * - Philippe Merle <philippe.merle@inria.fr>
+ * - Faiez Zalila <faiez.zalila@inria.fr>
+ */
+package org.eclipse.cmf.occi.multicloud.occimonitoring.provider;
+
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.cmf.occi.core.OCCIPackage;
+
+import org.eclipse.cmf.occi.core.provider.ResourceItemProvider;
+
+import org.eclipse.cmf.occi.multicloud.occimonitoring.OccimonitoringFactory;
+import org.eclipse.cmf.occi.multicloud.occimonitoring.OccimonitoringPackage;
+import org.eclipse.cmf.occi.multicloud.occimonitoring.Zabbixtemplate;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+/**
+ * This is the item provider adapter for a {@link org.eclipse.cmf.occi.multicloud.occimonitoring.Zabbixtemplate} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ZabbixtemplateItemProvider extends ResourceItemProvider {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ZabbixtemplateItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addZabbixtemplateIdentifierPropertyDescriptor(object);
+			addZabbixtemplateHostgidPropertyDescriptor(object);
+			addZabbixtemplateHostsidsPropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Zabbixtemplate Identifier feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addZabbixtemplateIdentifierPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_Zabbixtemplate_zabbixtemplateIdentifier_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_Zabbixtemplate_zabbixtemplateIdentifier_feature", "_UI_Zabbixtemplate_type"),
+				 OccimonitoringPackage.Literals.ZABBIXTEMPLATE__ZABBIXTEMPLATE_IDENTIFIER,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Zabbixtemplate Hostgid feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addZabbixtemplateHostgidPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_Zabbixtemplate_zabbixtemplateHostgid_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_Zabbixtemplate_zabbixtemplateHostgid_feature", "_UI_Zabbixtemplate_type"),
+				 OccimonitoringPackage.Literals.ZABBIXTEMPLATE__ZABBIXTEMPLATE_HOSTGID,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Zabbixtemplate Hostsids feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addZabbixtemplateHostsidsPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_Zabbixtemplate_zabbixtemplateHostsids_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_Zabbixtemplate_zabbixtemplateHostsids_feature", "_UI_Zabbixtemplate_type"),
+				 OccimonitoringPackage.Literals.ZABBIXTEMPLATE__ZABBIXTEMPLATE_HOSTSIDS,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This returns Zabbixtemplate.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/Zabbixtemplate"));
+	}
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String getText(Object object) {
+		String label = ((Zabbixtemplate)object).getId();
+		return label == null || label.length() == 0 ?
+			getString("_UI_Zabbixtemplate_type") :
+			getString("_UI_Zabbixtemplate_type") + " " + label;
+	}
+	
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(Zabbixtemplate.class)) {
+			case OccimonitoringPackage.ZABBIXTEMPLATE__ZABBIXTEMPLATE_IDENTIFIER:
+			case OccimonitoringPackage.ZABBIXTEMPLATE__ZABBIXTEMPLATE_HOSTGID:
+			case OccimonitoringPackage.ZABBIXTEMPLATE__ZABBIXTEMPLATE_HOSTSIDS:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+	 * that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add
+			(createChildParameter
+				(OCCIPackage.Literals.ENTITY__PARTS,
+				 OccimonitoringFactory.eINSTANCE.createMetrics()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(OCCIPackage.Literals.ENTITY__PARTS,
+				 OccimonitoringFactory.eINSTANCE.createCpuusage()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(OCCIPackage.Literals.ENTITY__PARTS,
+				 OccimonitoringFactory.eINSTANCE.createMemoryusage()));
+	}
+
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getResourceLocator() {
+		return OccimonitoringEditPlugin.INSTANCE;
+	}
+
+}
